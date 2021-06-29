d929f7ac6ee9696bd2333878d88bd60a792a0912
MdTasib <ohidulalam442@gmail.com>
2021-06-29 20:35:38 +0600

    project start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+/* eslint-disable react/jsx-pascal-case */
+import Module03 from "./Module_03/Module03";
+
 function App() {
   return (
     <div className="App">
-
+      <Module03 />
     </div>
   );
 }
---
+++ src/Module_03/Module03.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExpenseItem from './components/ExpenseItem';
+
+const Module03 = () => {
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            <ExpenseItem />
+        </div>
+    );
+};
+
+export default Module03;
