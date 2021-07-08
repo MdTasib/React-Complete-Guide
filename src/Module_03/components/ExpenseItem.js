ca51e98c125f43f06957a3cb6f7be3ffa28a48a7
MdTasib <ohidulalam442@gmail.com>
2021-07-08 11:29:58 +0600

    calander part done

--- src/Module_03/components/ExpenseItem.js
+++ src/Module_03/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+    const month = props.date.getMonth();
+    const day = props.date.getDay();
+    const year = props.date.getFullYear();
+
     return (
         <div className='expense-item'>
-            <div>{props.date.toDateString()}</div>
+            <div>
+                <div>Month:{month}</div>
+                <div>Year:{year}</div>
+                <div>Day:{day}</div>
+            </div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
