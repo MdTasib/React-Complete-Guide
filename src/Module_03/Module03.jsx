add636d0e985f91b7f1f03f5afb24f4fa020acee
MdTasib <ohidulalam442@gmail.com>
2021-07-15 22:55:47 +0600

    NewExpense & ExpenseForm

--- src/Module_03/Module03.jsx
+++ src/Module_03/Module03.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const Module03 = () => {
     return (
         <div>
-            <h2>Let's get started!</h2>
+            <NewExpense />
             <Expenses />
         </div>
     );
