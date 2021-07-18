5e002889b45567a2292358de39164756e3647672
MdTasib <ohidulalam442@gmail.com>
2021-07-18 22:25:08 +0600

    styleing component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 /* eslint-disable react/jsx-pascal-case */
 import Module03 from "./Module_03/Module03";
+import Styleing06 from './Styleing_06/Styleing06';
 
 function App() {
   return (
     <div className="App">
-      <Module03 />
+      {/* <Module03 /> */}
+      <Styleing06 />
     </div>
   );
 }
