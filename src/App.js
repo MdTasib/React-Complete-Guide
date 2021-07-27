88556fb062ff772ed22e276722cc8decdcac4414
MdTasib <ohidulalam442@gmail.com>
2021-07-27 11:00:59 +0600

    card ui component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-pascal-case */
 import Module03 from "./Module_03/Module03";
 import Styleing06 from './Styleing_06/Styleing06';
+import Module_08 from "./Module_08/Module_08";
 
 function App() {
   return (
     <div className="App">
       {/* <Module03 /> */}
-      <Styleing06 />
+      {/* <Styleing06 /> */}
+      <Module_08 />
     </div>
   );
 }
---
+++ src/Module_08/Module_08.jsx
@@ -0,0 +1,11 @@
+import AddUser from "./components/Users/AddUser";
+
+function Module_08() {
+  return (
+    <div>
+      <AddUser />
+    </div>
+  )
+}
+
+export default Module_08;
