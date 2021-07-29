5ffd2c683f109fbbcd6181b8d56e7f78c35d75a7
MdTasib <ohidulalam442@gmail.com>
2021-07-29 22:43:23 +0600

    project one done

--- src/Module_08/components/Users/AddUser.js
+++ src/Module_08/components/Users/AddUser.js
@@ -7,13 +7,22 @@ import ErrorModal from '../UI/ErrorModal';
 const AddUser = (props) => {
   const [enteredUserName, setEnteredUserName] = useState('');
   const [enteredUserAge, setEnteredUserAge] = useState('');
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUserName.trim().length === 0 || enteredUserAge.trim().length === 0) {
+      setError({
+        title: 'Invalid Input',
+        message: 'Plase inter a valid name and age (non-empty values).'
+      })
       return;
     }
     if (+enteredUserAge < 1) {
+      setError({
+        title: 'Invalid Age',
+        message: 'Plase inter a valid age (> 0).'
+      })
       return;
     }
     props.onAddUser(enteredUserName, enteredUserAge);
@@ -29,16 +38,20 @@ const AddUser = (props) => {
     setEnteredUserAge(event.target.value);
   }
 
+  const errorHandler = () => {
+    setError(null);
+  }
+
   return (
     <div>
-      <ErrorModal title='An error this' message='This an error' />
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
           <input type="text" id="username" value={enteredUserName} onChange={changeUsernameHandler} />
           <label htmlFor="age">Age (Years)</label>
           <input type="number" id="age" value={enteredUserAge} onChange={changeUserAgeHandler} />
-          <Button type="submit">Add User</Button>
+          <Button type="submit" onClick={errorHandler}>Add User</Button>
         </form>
       </Card>
     </div>
