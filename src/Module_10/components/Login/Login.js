a0b753e1339ee68e26aae10a5f903884aaa55afe
MdTasib <ohidulalam442@gmail.com>
2021-08-24 10:51:41 +0600

    useEffect & dependenci -- module 10 - 04

--- src/Module_10/components/Login/Login.js
+++ src/Module_10/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,12 +11,14 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(() => {
+    setFormIsValid(
+      enteredEmail.includes('@') && enteredPassword.trim().length > 6
+    )
+  }, [enteredPassword, enteredEmail]);
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
@@ -46,7 +48,7 @@ const Login = (props) => {
         <div
           className={`${classes.control} ${
             emailIsValid === false ? classes.invalid : ''
-          }`}
+            }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
@@ -60,7 +62,7 @@ const Login = (props) => {
         <div
           className={`${classes.control} ${
             passwordIsValid === false ? classes.invalid : ''
-          }`}
+            }`}
         >
           <label htmlFor="password">Password</label>
           <input
