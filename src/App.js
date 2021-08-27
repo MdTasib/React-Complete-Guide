fcc73c7d8958bb29830893c676ca70b98a902cc7
MdTasib <ohidulalam442@gmail.com>
2021-08-27 18:34:06 +0600

    module 10 - lecture - 17 - custom context provider

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Module03 from "./Module_03/Module03";
 import Styleing06 from './Styleing_06/Styleing06';
 import Module_08 from "./Module_08/Module_08";
 import Module_10 from './Module_10/Module_10';
+import { AuthContextProvider } from "./Module_10/store/auth-context";
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
       {/* <Module03 /> */}
       {/* <Styleing06 /> */}
       {/* <Module_08 /> */}
-      <Module_10 />
+
+      <AuthContextProvider><Module_10 /></AuthContextProvider>
+
     </div>
   );
 }
