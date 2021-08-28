9f5274b2038fcb6d1b2e11f6f8a77e3721927ad5
MdTasib <ohidulalam442@gmail.com>
2021-08-28 10:28:48 +0600

    module 11 - setup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Styleing06 from './Styleing_06/Styleing06';
 import Module_08 from "./Module_08/Module_08";
 import Module_10 from './Module_10/Module_10';
 import { AuthContextProvider } from "./Module_10/store/auth-context";
+import Module_11 from "./Module_11/Module_11";
 
 function App() {
   return (
@@ -12,9 +13,8 @@ function App() {
       {/* <Module03 /> */}
       {/* <Styleing06 /> */}
       {/* <Module_08 /> */}
-
-      <AuthContextProvider><Module_10 /></AuthContextProvider>
-
+      {/* <AuthContextProvider><Module_10 /></AuthContextProvider> */}
+      <Module_11 />
     </div>
   );
 }
