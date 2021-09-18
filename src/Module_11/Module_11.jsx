ed50d3b8678b0b5b62cc98d4979d00275ad1739b
MdTasib <ohidulalam442@gmail.com>
2021-09-18 11:26:22 +0600

    project 11 setup

--- src/Module_11/Module_11.jsx
+++ src/Module_11/Module_11.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Module_11 = () => {
-  return (
-    <div>
-      <h2>Project 11</h2>
-    </div>
-  );
+	return (
+		<div>
+			<h2>Project 11</h2>
+		</div>
+	);
 };
 
 export default Module_11;
