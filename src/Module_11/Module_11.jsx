494cc3bc83342380c85a579fe0d8ba1bbb7eac2f
MdTasib <ohidulalam442@gmail.com>
2021-09-19 11:33:07 +0600

    meals component adding

--- src/Module_11/Module_11.jsx
+++ src/Module_11/Module_11.jsx
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 import Header from './Components/Layout/Header';
+import Meals from './Components/Meals/Meals';
 
 const Module_11 = () => {
 	return (
 		<Fragment>
 			<Header />
+			<main>
+				<Meals />
+			</main>
 		</Fragment>
 	);
 };
