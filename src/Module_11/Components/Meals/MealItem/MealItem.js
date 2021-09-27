2a7157ead3eade669f9c103edf1cf1b16822fabf
MdTasib <ohidulalam442@gmail.com>
2021-09-27 10:51:48 +0600

    working with refs and forwordRefs

--- src/Module_11/Components/Meals/MealItem/MealItem.js
+++ src/Module_11/Components/Meals/MealItem/MealItem.js
@@ -1,8 +1,20 @@
 import classes from './MealItem.module.css';
 import MealItemForm from './MealltemForm';
+import { useContext } from 'react';
+import CartContext from '../../../Store/cart-context';
 
 const MealItem = props => {
+	const cartCtx = useContext(CartContext);
 	const price = `$${props.price.toFixed(2)}`;
+
+	const addToCartHandler = amount => {
+		cartCtx.addItem({
+			id: props.id,
+			price: props.price,
+			name: props.name,
+			amount: amount,
+		});
+	};
 
 	return (
 		<li className={classes.meal}>
@@ -12,7 +24,7 @@ const MealItem = props => {
 				<div className={classes.price}>{price}</div>
 			</div>
 			<div>
-				<MealItemForm />
+				<MealItemForm onAddToCart={addToCartHandler} />
 			</div>
 		</li>
 	);
