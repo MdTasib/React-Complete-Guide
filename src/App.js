d75c6ba959859965d06ff62513d76a875ce7bd95
MdTasib <ohidulalam442@gmail.com>
2021-10-02 18:32:32 +0600

    module 12 - lecture-02

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-pascal-case */
-import Module03 from "./Module_03/Module03";
+import Module03 from './Module_03/Module03';
 import Styleing06 from './Styleing_06/Styleing06';
-import Module_08 from "./Module_08/Module_08";
+import Module_08 from './Module_08/Module_08';
 import Module_10 from './Module_10/Module_10';
-import { AuthContextProvider } from "./Module_10/store/auth-context";
-import Module_11 from "./Module_11/Module_11";
+import { AuthContextProvider } from './Module_10/store/auth-context';
+import Module_11 from './Module_11/Module_11';
+import Module_12 from './Module_12/Module_12';
 
 function App() {
-  return (
-    <div className="App">
-      {/* <Module03 /> */}
-      {/* <Styleing06 /> */}
-      {/* <Module_08 /> */}
-      {/* <AuthContextProvider><Module_10 /></AuthContextProvider> */}
-      <Module_11 />
-
-
-    </div>
-  );
+	return (
+		<div className='App'>
+			{/* <Module03 /> */}
+			{/* <Styleing06 /> */}
+			{/* <Module_08 /> */}
+			{/* <AuthContextProvider><Module_10 /></AuthContextProvider> */}
+			{/* <Module_11 /> */}
+			<Module_12 />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Module_12/Module_12.jsx
@@ -0,0 +1,5 @@
+const Module_12 = () => {
+	return <div>hello</div>;
+};
+
+export default Module_12;
