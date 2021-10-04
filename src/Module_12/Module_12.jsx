323cc4906291c4aa10a80f6c6cb3f97f6603ea25
MdTasib <ohidulalam442@gmail.com>
2021-10-04 09:34:45 +0600

    module-12 - lecture - 03

--- src/Module_12/Module_12.jsx
+++ src/Module_12/Module_12.jsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+import './module_12.css';
+import Button from './components/UI/Button.js';
+
 const Module_12 = () => {
-	return <div>hello</div>;
+	const [showParagraph, setShowParagraph] = useState(false);
+
+	console.log('Toggle');
+
+	const toggleParagraphHandler = () => {
+		setShowParagraph(prevShowParagraph => !prevShowParagraph);
+	};
+
+	return (
+		<div className='app'>
+			<h1>Hi there!</h1>
+			{showParagraph && <p>This is new!</p>}
+			<Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+		</div>
+	);
 };
 
 export default Module_12;
