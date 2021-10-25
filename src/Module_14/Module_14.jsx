0ac436e04d185a9c6ce0772632b0b536d6627846
MdTasib <ohidulalam442@gmail.com>
2021-10-25 10:01:53 +0600

    database connected & module 14 done

--- src/Module_14/Module_14.jsx
+++ src/Module_14/Module_14.jsx
@@ -13,7 +13,9 @@ const Module_14 = () => {
 		setError(null);
 
 		try {
-			const response = await fetch('https://swapi.dev/api/films');
+			const response = await fetch(
+				'https://first-firebase-practice-default-rtdb.firebaseio.com/movie.json'
+			);
 
 			if (!response.ok) {
 				throw new Error('Something went wrong');
@@ -21,15 +23,18 @@ const Module_14 = () => {
 
 			const data = await response.json();
 
-			const transformedMovies = data.results.map(movieData => {
-				return {
-					id: movieData.episode_id,
-					title: movieData.title,
-					openingText: movieData.opening_crawl,
-					releaseDate: movieData.release_date,
-				};
-			});
-			setMovies(transformedMovies);
+			const loadedMovies = [];
+
+			for (const key in data) {
+				loadedMovies.push({
+					id: key,
+					title: data[key].title,
+					openingText: data[key].openingText,
+					releaseDate: data[key].releaseDate,
+				});
+			}
+
+			setMovies(loadedMovies);
 		} catch (error) {
 			setError(error.message);
 		}
@@ -40,8 +45,17 @@ const Module_14 = () => {
 		fetchMoviesHandler();
 	}, [fetchMoviesHandler]);
 
-	function addMovieHandler(movie) {
-		console.log(movie);
+	async function addMovieHandler(movie) {
+		const response = await fetch(
+			'https://first-firebase-practice-default-rtdb.firebaseio.com/movie.json',
+			{
+				method: 'POST',
+				body: JSON.stringify(movie),
+				headers: { 'Content-Type': 'application/json' },
+			}
+		);
+		const data = await response.json();
+		console.log(data);
 	}
 
 	let content = <p>Found No Movies</p>;
