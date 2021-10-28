179f7d4fe5025a6fb546e29e98197a8dac020cd7
MdTasib <ohidulalam442@gmail.com>
2021-10-28 21:16:15 +0600

    create custom hook

--- src/module_15/module_15.jsx
+++ src/module_15/module_15.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import ForwardCounter from './components/ForwardCounter';
+import BackwardCounter from './components/BackwardCounter';
 
 const Module_15 = () => {
-	return <div>Module_15</div>;
+	return (
+		<React.Fragment>
+			<ForwardCounter />
+			<BackwardCounter />
+		</React.Fragment>
+	);
 };
 
 export default Module_15;
